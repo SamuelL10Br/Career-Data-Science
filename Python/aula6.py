56957bedc8afaf840e6867ea98db1a6d2116766f
Samuel Luciano <108229765+SamuelL10Br@users.noreply.github.com>
2025-12-26 17:13:00 -0300

    Update aula6.py

--- Python/aula6.py
+++ Python/aula6.py
@@ -23,3 +23,15 @@ for usuario in usuarios:
     if usuario["idade"] >= 18:
         print(usuario["nome"], "é maior de idade")
 
+#Atividade prática para acessar dados
+dicionarios = [
+    {"nome":"Samuel","idade":22,"profissão":"Cientista De Dados e Segurança"},
+    {"nome":"Victor","idade":16,"profissão":"Front-End Design"},
+    {"nome":"Lívia","idade":48,"profissão":"UX-UI Design"}
+]
+
+for usuario in dicionarios:
+    if usuario["idade"] >= 18:
+        print(usuario["nome"],"é maior de idade")
+    else:
+        print(usuario["nome"],"Não é maior de idade")
